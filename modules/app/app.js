e16398c913a413eacda5816c85c89c9311027253
aniledev <elinamcgill@gmail.com>
2021-03-03 10:28:59 -0500

    CHORE: set up express modules and middleware

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,13 +1,29 @@
-const express = require('express')
-const cors = require('cors')
+// MODULES AND PACKAGES
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const winston = require("winston");
+const { NODE_ENV, PORT } = require("../../config");
+const errorHandler = require("../../error-handler");
+const logger = require("../../logger");
+const app = express();
 
-const app = express()
+// MIDDLEWARE 
+const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
 
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-app.use('/people', require('../people/people.router'))
-app.use('/pets', require('../pets/pets.router'))
-
+// ROUTES
+app.use("/people", require("../people/people.router"));
+app.use("/pets", require("../pets/pets.router"));
 // add a get endpoint that returns the string hello world
 
-module.exports = app
+// ERROR HANDLING MIDDLEWARE
+app.use(errorHandler);
+
+module.exports = app;
