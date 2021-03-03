39a7317c15f9987e002a202d883ab16e7e168103
aniledev <elinamcgill@gmail.com>
2021-03-03 10:21:23 -0500

    CHORE: add pseudocode for app get endpoint

--- modules/app/app.js
+++ modules/app/app.js
@@ -8,4 +8,6 @@ app.use(cors())
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
 
+// add a get endpoint that returns the string hello world
+
 module.exports = app
