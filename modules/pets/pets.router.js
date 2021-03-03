6963b5c0dfd9a8d033c113b92a0940d578cb9cc6
aniledev <elinamcgill@gmail.com>
2021-03-03 10:31:53 -0500

    FEAT: add endpoint paths to router

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -6,29 +6,29 @@ const People = require('../people/people.service')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/api/pets', (req, res) => {
   // Return all pets currently up for adoption.
 })
 
-router.get('/', (req, res) => {
+router.get('/api/cats', (req, res) => {
   // Return all cats currently up for adoption.
 })
 
-router.get('/', (req, res) => {
+router.get('/api/dogs', (req, res) => {
   // Return all dogs currently up for adoption.
 })
 
 
 
-router.delete('/', json, (req, res) => {
+router.delete('/api/pets', json, (req, res) => {
   // Remove a pet from adoption.
 })
 
-router.delete('/', json, (req, res) => {
+router.delete('/api/cats', json, (req, res) => {
   // Remove a cat from adoption.
 })
 
-router.delete('/', json, (req, res) => {
+router.delete('/api/dogs', json, (req, res) => {
   // Remove a dog from adoption.
 })
 
