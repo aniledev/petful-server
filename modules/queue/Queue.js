34cd676f479710b8f6a109263b1dfcf82e8e94cb
aniledev <elinamcgill@gmail.com>
2021-03-03 16:55:06 -0500

    FEAT: write method to add a node to the queue depending upon the state of the queue

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -13,8 +13,21 @@ class Queue {
     this.last = null;
   }
 
-  enqueue(data) {
-    // Add some data to the queue.
+  enqueue(item) {
+    // Add new data/node to the queue.
+    const node = new _Node(item);
+
+    // if the the first node is empty
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    // if there is an node in the queue, let the last one point to the new node
+    if (this.last) {
+      this.last.next = node;
+    }
+
+    this.last = node;
   }
 
   dequeue() {
