07161e7d42b6d17e92ac2757dd502a3136dccbba
aniledev <elinamcgill@gmail.com>
2021-03-03 10:36:40 -0500

    CHORE: write pseudocode for router endpoints

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -1,37 +1,39 @@
-const express = require('express')
-const json = require('body-parser').json()
+const express = require("express");
+const json = require("body-parser").json();
 
-const Pets = require('./pets.service')
-const People = require('../people/people.service')
+const Pets = require("./pets.service");
+const People = require("../people/people.service");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/api/pets', (req, res) => {
-  // Return all pets currently up for adoption.
-})
+router.get("/api/pets", (req, res) => {
+  // Return all pets currently up for adoption, entire array in store module in a json format
+  // status code of 200, success
+});
 
-router.get('/api/cats', (req, res) => {
-  // Return all cats currently up for adoption.
-})
+router.get("/api/cats", (req, res) => {
+  // Use the filter method to return all cats currently up for adoption in the cats array in a json format
+  // status code of 200, success
+});
 
-router.get('/api/dogs', (req, res) => {
-  // Return all dogs currently up for adoption.
-})
+router.get("/api/dogs", (req, res) => {
+  // Use the filter method to return all dogs currently up for adoption in the cats array in a json format
+  // status code of 200, success
+});
 
+router.delete("/api/pets", json, (req, res) => {
+  // Remove a pet from adoption, first in first out, you can only delete the first item in the array, delete the first pet
+  // status code of 204??, end() ??
+});
 
+router.delete("/api/cats", json, (req, res) => {
+  // Remove a cat from adoption, first in first out, you can only delete the first item in the array, delete the first cat
+  // status code of 204??, end() ??
+});
 
-router.delete('/api/pets', json, (req, res) => {
-  // Remove a pet from adoption.
-})
-
-router.delete('/api/cats', json, (req, res) => {
-  // Remove a cat from adoption.
-})
-
-router.delete('/api/dogs', json, (req, res) => {
-  // Remove a dog from adoption.
-})
-
-
+router.delete("/api/dogs", json, (req, res) => {
+   // Remove a dog from adoption, first in first out, you can only delete the first item in the array, delete the first dog
+  // status code of 204??, end() ??
+});
 
 module.exports = router;
