de9198ec898ec67b11369d7e98b5e8048f82fb71
aniledev <elinamcgill@gmail.com>
2021-03-03 21:52:18 -0500

    FEAT: create cats router and service modules

---
+++ modules/cats/cats.router.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const json = require("body-parser").json();
+const store = require("../../store");
+
+const Cats = require("./cats.service");
+const People = require("../people/people.service");
+
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  // Use the filter method to return all cats currently up for adoption in the cats array in a json format
+  // status code of 200, success
+  res.status(200).json(store);
+});
+
+router.delete("/", json, (req, res) => {
+  // Remove a cat from adoption, first in first out, you can only delete the first item in the array, delete the first cat
+  // status code of 204??, end() ??
+});
+
+module.exports = catsRouter;
