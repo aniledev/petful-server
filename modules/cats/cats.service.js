43657610c2de6a7f541bdfa28e6b51260a673486
aniledev <elinamcgill@gmail.com>
2021-03-03 22:48:14 -0500

    FEAT: add getCats and dequeueCats function to cats service

--- modules/cats/cats.service.js
+++ modules/cats/cats.service.js
@@ -1,23 +1,31 @@
 const Queue = require("../queue/Queue");
 const store = require("../../store");
+const returnAll = require("../queue/Queue");
 
 // Set up initial data.
 // --------------------
 
-const pets = {
-  cats: new Queue(),
-};
+// create a new cats queue using the data structure we created
+let Cats = new Queue();
 
-store.cats.forEach((cat) => pets.cats.enqueue(cat));
+// for each cat in the array in the store, add the cat to the queue data structure
+store.cats.forEach((cat) => Cats.enqueue(cat));
 
 // --------------------
 
 module.exports = {
-  get() {
-    // Return the pets next in line to be adopted.
+  getCats() {
+    // Returns the cat array using the methods in the queue class
+    const getCats = returnAll(Cats);
+    return getCats;
   },
 
-  dequeue(type) {
-    // Remove a pet from the queue.
+  dequeueCats() {
+    // Remove a cat from the cat queue
+    const adoptedCat = Cats.dequeue();
+
+    // add the adopted cat back to the end of the queue so the queue is never empty
+    Cats.enqueue(adoptedCat);
+    return returnAll(Cats);
   },
 };
