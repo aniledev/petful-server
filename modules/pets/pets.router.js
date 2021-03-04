5d0578054864c72cc42f9e0e0d5d77969af873e8
aniledev <elinamcgill@gmail.com>
2021-03-03 21:52:44 -0500

    FEAT: create people router and service modules

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -13,31 +13,9 @@ router.get("/pets", (req, res) => {
   res.status(200).json(store);
 });
 
-router.get("/cats", (req, res) => {
-  // Use the filter method to return all cats currently up for adoption in the cats array in a json format
-  // status code of 200, success
-  res.status(200).json(store);
-});
-
-router.get("/dogs", (req, res) => {
-  // Use the filter method to return all dogs currently up for adoption in the cats array in a json format
-  // status code of 200, success
-  res.status(200).json(store);
-});
-
 router.delete("/pets", json, (req, res) => {
   // Remove a pet from adoption, first in first out, you can only delete the first item in the array, delete the first pet
   // status code of 204??, end() ??
 });
 
-router.delete("/cats", json, (req, res) => {
-  // Remove a cat from adoption, first in first out, you can only delete the first item in the array, delete the first cat
-  // status code of 204??, end() ??
-});
-
-router.delete("/dogs", json, (req, res) => {
-  // Remove a dog from adoption, first in first out, you can only delete the first item in the array, delete the first dog
-  // status code of 204??, end() ??
-});
-
-module.exports = router;
+module.exports = petsRouter;
