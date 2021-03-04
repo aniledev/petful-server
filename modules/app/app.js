ecc8bf9d462b235fcaecd4f12d6fae3898332557
aniledev <elinamcgill@gmail.com>
2021-03-04 02:02:17 -0500

    REFACTOR: remove references to deleted code

--- modules/app/app.js
+++ modules/app/app.js
@@ -6,7 +6,6 @@ const helmet = require("helmet");
 const cors = require("cors");
 const winston = require("winston");
 const { NODE_ENV, PORT } = require("../../config");
-const petsRouter = require("../pets/pets.router");
 const catsRouter = require("../cats/cats.router");
 const dogsRouter = require("../dogs/dogs.router");
 const peopleRouter = require("../people/people.router");
@@ -25,7 +24,6 @@ app.use(cors());
 app.use(express.json());
 
 // ROUTES
-app.use("/api/pets", petsRouter);
 app.use("/api/cats", catsRouter);
 app.use("/api/dogs", dogsRouter);
 app.use("/api/people", peopleRouter);
