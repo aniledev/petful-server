4703807015a17a2ccc7bfd1133406e03df703b39
aniledev <elinamcgill@gmail.com>
2021-03-11 03:34:01 -0500

    FEAT: implement POST request for people router

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -16,6 +16,14 @@ peopleRouter
     // send back the new queue of people in json data
     res.status(200).json(People.dequeuePerson());
   })
-});
+  .post(json, (req, res) => {
+    // access the req body
+    const { name } = req.body;
+    // const newPerson = { name };
+    // Add a new person to the queue.
+    const newPeopleQueue = People.enqueuePerson(name);
+    // send back json data of the new queue
+    res.status(201).json(newPeopleQueue);
+  });
 
 module.exports = peopleRouter;
